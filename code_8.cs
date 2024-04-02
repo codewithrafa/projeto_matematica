85e4c44912b145ebfbae99d84b9a18cd260886c2
Rafael Carvalho Silva <rafael.silva200506@gmail.com>
2024-04-02 19:10:26 -0300

    NOVO CÓDIGO - 02/04/2024 -O meu 8° código exerce a função de calcular o fatorial de qualquer número que o usuário digitar.

---
+++ code_8.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projeto_matematica
+{
+    internal class code_8
+    {
+   
+        public static double CalcFator(double f)
+        {
+            double fatorial = 1;
+            for (int i = 1; i <= f; i++)
+            {
+                fatorial *= i;
+            }
+            return fatorial;
+        }
+
+        public void Fatorial()
+        {
+            char cont = 's';
+            while (cont == 's')
+            {
+
+                Console.WriteLine("Olá. Aqui iremos calcular o fatorial de qualquer número.\n");
+
+                Console.WriteLine("Digite o número para o cálculo do fatorial: \n");
+                double f = int.Parse(Console.ReadLine());
+
+                if (f == 0)
+                {
+                    Console.WriteLine("O fatorial de 0 é 1");
+                }
+                else
+                {
+                    Console.WriteLine($"O fatorial do número {f} é {CalcFator(f)}");
+                }
+
+                Console.WriteLine("Deseja realizar outro cálculo? s/n");
+                cont = char.Parse(Console.ReadLine());
+                Console.Clear();
+            }
+        }
+    }
+}
